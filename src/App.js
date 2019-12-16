2d3903822b944040e8edccaf3cc35b5d8ff2c2fb
Zach Capshaw <zcapshaw@gmail.com>
2019-12-16 07:47:38 -0700

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import background from './assets/images/background.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header">
+          <div className="title-text">
+            <h1 className="title-text">Hello, my name is Zach.</h1>
+            <h4>I design and build software.</h4>
+          </div>
+        </div>
       </header>
     </div>
   );
