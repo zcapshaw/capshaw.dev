9c967a7ef9171345b4b5ce8974716f1c4d2a22c0
Zach Capshaw <zcapshaw@gmail.com>
2019-12-28 10:11:09 -0700

    fix responsive styling on AboutMe

--- src/screens/AboutMe.js
+++ src/screens/AboutMe.js
@@ -6,20 +6,20 @@ class AboutMe extends Component {
   render() {
     return (
         <section className="about">
-          <div className="bg-gray-400" style={{ height: "80vh" }}> 
+          <div className="bg-gray-800 pb-32 lg:pb-64"> 
             <div className="pt-32 pb-8">
-                <h1 id="AboutMe" className="font-bold text-center uppercase text-blue-900 tracking-wide antialiased">About Me</h1> 
+                <h2 id="AboutMe" className="font-bold text-center uppercase text-white tracking-wide antialiased lg:text-4xl">About Me</h2> 
             </div>
-            <div className="flex max-w-2xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden mb-16">
-                <div className="flex m-auto justify-center">
-                    <img src={headshot} alt="Zach" className="rounded-full w-7/12"/>
+            <div className="lg:flex max-w-md lg:max-w-2xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden mb-16">
+                <div className="flex m-auto py-8 lg:mx-8 justify-center w-40 lg:w-1/3">
+                    <img src={headshot} alt="Zach" className="rounded-full"/>
                 </div>
-                <div className="m-auto pr-12 pb-12 pt-8 max-w-3xl">
+                <div className="m-auto px-8 pb-8 lg:py-8 max-w-3xl">
                     <p className="text-xl text-gray-800">I'm an experienced Product Manager, UX Designer, and Developer. I've worked in Product Devlopment at multiple startups and as a consultant helping clients design, build, and launch apps. </p>
                 </div>
             </div>
           </div>
-          <div className="flex max-w-6xl mx-auto bg-white -mt-64 mb-16 shadow-lg rounded-lg overflow-hidden">
+          <div className="flex flex-col max-w-md lg:max-w-5xl lg:flex-row mx-auto bg-white -mt-32 lg:-mt-64 mb-16 shadow-lg rounded-lg overflow-hidden">
                 <div className="m-auto p-12 pb-12 pt-8 max-w-3xl">
                     <h1 className="text-blue-800 font-bold text-center">Designer</h1>
                     <p className="text-xl text-gray-800 py-4">I'm an experienced Product Manager, UX Designer, and Developer. I've worked in Product Devlopment at multiple startups and as a consultant helping clients design, build, and launch apps. </p>
