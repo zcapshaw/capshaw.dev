d93080b5c9abb4cbff163a1c928da5be465b04c0
Zach Capshaw <zcapshaw@gmail.com>
2019-12-27 19:09:50 -0700

    componentize icons and social links

--- src/App.js
+++ src/App.js
@@ -1,101 +1,58 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown, faBars } from '@fortawesome/free-solid-svg-icons'
-import {faGithub, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import background from './assets/images/background.jpg';
+import React, { Component } from 'react';
+import HomeScreen from './screens/HomeScreen';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App" >
-      <div  className="flex bg-cover bg-center bg-fixed h-screen w-screen relative" 
-            style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url(${background})`}}>
-        <div className="top-0 right-0 fixed mt-6 mr-6">
-            <FontAwesomeIcon className="center" icon={faBars} color="#BBBBBB" size="lg"/>
-        </div>  
-        <div className="m-auto">
-          <h1 className="md:text-5xl text-white font-bold antialiased">
-            Hello, my name is Zach.
-          </h1>
-          <h2 className="text-white font-test antialiased">
-            I design and build software.
-          </h2>
-          <div className="my-8">
-            <button className="btn hover:bg-gray-700 focus:outline-none font-bold antialiased">
-              View my work
-            </button>   
-            <button className="btn hover:bg-gray-700 focus:outline-none ml-6 font-bold antialiased">
-              Contact me
-            </button>   
+  render() {
+    return (
+      <div className="App" >
+        <HomeScreen />
+        <section className="about">
+          <div> 
+            <div className="my-16">
+              <h1 className="font-bold text-center uppercase text-gray-700 tracking-wide antialiased">About Me</h1> 
+            </div>      
+            <div className="mx-8 sm:mx-16 pb-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsa repellendus sequi, rem labore consectetur omnis quis perspiciatis facere neque quisquam autem non aspernatur, sit porro quia explicabo amet illum.</p>
+              <p>Quidem excepturi beatae sint harum corrupti perferendis aliquid facere enim velit suscipit, tempora blanditiis, rem nam ullam soluta consequuntur ipsa aliquam fuga eius assumenda nemo, facilis sapiente reiciendis quisquam. Vel?</p>
+              <p>Tenetur eaque, rerum dolores perferendis accusantium nemo, eius natus minima non adipisci quisquam quos iusto ipsam vel ducimus! Quo suscipit ipsa consequatur facilis veniam, perferendis nulla odit consectetur tenetur nostrum.</p>
+              <p>Quidem dolores atque sapiente sequi aut ducimus consequatur voluptatem aperiam! Rerum, amet. Aliquid esse odit id architecto sunt cumque, eius sit necessitatibus? Quis voluptatum harum maxime explicabo, illum voluptatem at!</p>
+              <p>Ducimus, amet tempore placeat debitis tempora iure earum, cumque officiis provident laudantium nemo sunt dolorum. Omnis harum rem, aliquam nam dignissimos quo autem repellendus iusto labore quam deserunt laudantium accusamus!</p>
+            </div>
           </div>
-        </div> 
-        <div className="flex w-screen justify-center mb-6 bottom-0 absolute">
-            <FontAwesomeIcon className="center" icon={faChevronDown} color="#BBBBBB" size="lg"/>
-        </div>
-        <div className="sm:mr-8 absolute right-0 bottom-0 ">
-          <ul>
-            <li className="my-6 mx-4 inline-block">
-              <a href="https://github.com/zcapshaw" target="_blank" rel='noreferrer noopener' className='icon'>
-                <FontAwesomeIcon className="center" icon={faGithub} size="lg"/> 
-              </a>
-            </li>
-            <li className="my-6 mx-4 inline-block">
-              <a href="https://www.linkedin.com/in/zcapshaw/" target="_blank" rel='noreferrer noopener' className='icon'>
-                  <FontAwesomeIcon className="center" icon={faLinkedin} size="lg"/>
-              </a>
-            </li>
-            <li className="my-6 mx-4 inline-block">
-              <a href="https://www.instagram.com/zacapshaw/" target="_blank" rel='noreferrer noopener' className='icon'>
-                <FontAwesomeIcon className="center" icon={faInstagram} size="lg"/>
-              </a>
-            </li>
-          </ul>
-        </div>    
+        </section>
+        <section className="Projects">
+          <div className="bg-gray-300 h-full"> 
+            <div>
+              <h1 className="p-4 pt-16 font-bold text-center uppercase text-gray-700 tracking-wide antialiased">Projects</h1> 
+            </div>      
+            <div className="mx-8 sm:mx-16 pb-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsa repellendus sequi, rem labore consectetur omnis quis perspiciatis facere neque quisquam autem non aspernatur, sit porro quia explicabo amet illum.</p>
+              <p>Quidem excepturi beatae sint harum corrupti perferendis aliquid facere enim velit suscipit, tempora blanditiis, rem nam ullam soluta consequuntur ipsa aliquam fuga eius assumenda nemo, facilis sapiente reiciendis quisquam. Vel?</p>
+              <p>Tenetur eaque, rerum dolores perferendis accusantium nemo, eius natus minima non adipisci quisquam quos iusto ipsam vel ducimus! Quo suscipit ipsa consequatur facilis veniam, perferendis nulla odit consectetur tenetur nostrum.</p>
+              <p>Quidem dolores atque sapiente sequi aut ducimus consequatur voluptatem aperiam! Rerum, amet. Aliquid esse odit id architecto sunt cumque, eius sit necessitatibus? Quis voluptatum harum maxime explicabo, illum voluptatem at!</p>
+              <p>Ducimus, amet tempore placeat debitis tempora iure earum, cumque officiis provident laudantium nemo sunt dolorum. Omnis harum rem, aliquam nam dignissimos quo autem repellendus iusto labore quam deserunt laudantium accusamus!</p>
+            </div>
+          </div>
+        </section>
+        <section className="Contact">
+          <div className="bg-gray-900 h-full"> 
+            <div>
+              <h1 className=" p-4 pt-16 font-bold text-center uppercase text-white tracking-wide antialiased">Contact Me</h1> 
+            </div>      
+            <div className="mx-8 sm:mx-16 text-gray-500 pb-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsa repellendus sequi, rem labore consectetur omnis quis perspiciatis facere neque quisquam autem non aspernatur, sit porro quia explicabo amet illum.</p>
+              <p>Quidem excepturi beatae sint harum corrupti perferendis aliquid facere enim velit suscipit, tempora blanditiis, rem nam ullam soluta consequuntur ipsa aliquam fuga eius assumenda nemo, facilis sapiente reiciendis quisquam. Vel?</p>
+              <p>Tenetur eaque, rerum dolores perferendis accusantium nemo, eius natus minima non adipisci quisquam quos iusto ipsam vel ducimus! Quo suscipit ipsa consequatur facilis veniam, perferendis nulla odit consectetur tenetur nostrum.</p>
+              <p>Quidem dolores atque sapiente sequi aut ducimus consequatur voluptatem aperiam! Rerum, amet. Aliquid esse odit id architecto sunt cumque, eius sit necessitatibus? Quis voluptatum harum maxime explicabo, illum voluptatem at!</p>
+              <p>Ducimus, amet tempore placeat debitis tempora iure earum, cumque officiis provident laudantium nemo sunt dolorum. Omnis harum rem, aliquam nam dignissimos quo autem repellendus iusto labore quam deserunt laudantium accusamus!</p>
+            </div>
+          </div>
+        </section>
       </div>
-      <section className="about">
-        <div> 
-          <div className="my-16">
-            <h1 className="font-bold text-center uppercase text-gray-700 tracking-wide antialiased">About Me</h1> 
-          </div>      
-          <div className="mx-8 sm:mx-16 pb-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsa repellendus sequi, rem labore consectetur omnis quis perspiciatis facere neque quisquam autem non aspernatur, sit porro quia explicabo amet illum.</p>
-            <p>Quidem excepturi beatae sint harum corrupti perferendis aliquid facere enim velit suscipit, tempora blanditiis, rem nam ullam soluta consequuntur ipsa aliquam fuga eius assumenda nemo, facilis sapiente reiciendis quisquam. Vel?</p>
-            <p>Tenetur eaque, rerum dolores perferendis accusantium nemo, eius natus minima non adipisci quisquam quos iusto ipsam vel ducimus! Quo suscipit ipsa consequatur facilis veniam, perferendis nulla odit consectetur tenetur nostrum.</p>
-            <p>Quidem dolores atque sapiente sequi aut ducimus consequatur voluptatem aperiam! Rerum, amet. Aliquid esse odit id architecto sunt cumque, eius sit necessitatibus? Quis voluptatum harum maxime explicabo, illum voluptatem at!</p>
-            <p>Ducimus, amet tempore placeat debitis tempora iure earum, cumque officiis provident laudantium nemo sunt dolorum. Omnis harum rem, aliquam nam dignissimos quo autem repellendus iusto labore quam deserunt laudantium accusamus!</p>
-          </div>
-        </div>
-      </section>
-      <section className="Projects">
-        <div className="bg-gray-300 h-full"> 
-          <div>
-            <h1 className="p-4 pt-16 font-bold text-center uppercase text-gray-700 tracking-wide antialiased">Projects</h1> 
-          </div>      
-          <div className="mx-8 sm:mx-16 pb-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsa repellendus sequi, rem labore consectetur omnis quis perspiciatis facere neque quisquam autem non aspernatur, sit porro quia explicabo amet illum.</p>
-            <p>Quidem excepturi beatae sint harum corrupti perferendis aliquid facere enim velit suscipit, tempora blanditiis, rem nam ullam soluta consequuntur ipsa aliquam fuga eius assumenda nemo, facilis sapiente reiciendis quisquam. Vel?</p>
-            <p>Tenetur eaque, rerum dolores perferendis accusantium nemo, eius natus minima non adipisci quisquam quos iusto ipsam vel ducimus! Quo suscipit ipsa consequatur facilis veniam, perferendis nulla odit consectetur tenetur nostrum.</p>
-            <p>Quidem dolores atque sapiente sequi aut ducimus consequatur voluptatem aperiam! Rerum, amet. Aliquid esse odit id architecto sunt cumque, eius sit necessitatibus? Quis voluptatum harum maxime explicabo, illum voluptatem at!</p>
-            <p>Ducimus, amet tempore placeat debitis tempora iure earum, cumque officiis provident laudantium nemo sunt dolorum. Omnis harum rem, aliquam nam dignissimos quo autem repellendus iusto labore quam deserunt laudantium accusamus!</p>
-          </div>
-        </div>
-      </section>
-      <section className="Contact">
-        <div className="bg-gray-900 h-full"> 
-          <div>
-            <h1 className=" p-4 pt-16 font-bold text-center uppercase text-white tracking-wide antialiased">Contact Me</h1> 
-          </div>      
-          <div className="mx-8 sm:mx-16 text-gray-500 pb-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsa repellendus sequi, rem labore consectetur omnis quis perspiciatis facere neque quisquam autem non aspernatur, sit porro quia explicabo amet illum.</p>
-            <p>Quidem excepturi beatae sint harum corrupti perferendis aliquid facere enim velit suscipit, tempora blanditiis, rem nam ullam soluta consequuntur ipsa aliquam fuga eius assumenda nemo, facilis sapiente reiciendis quisquam. Vel?</p>
-            <p>Tenetur eaque, rerum dolores perferendis accusantium nemo, eius natus minima non adipisci quisquam quos iusto ipsam vel ducimus! Quo suscipit ipsa consequatur facilis veniam, perferendis nulla odit consectetur tenetur nostrum.</p>
-            <p>Quidem dolores atque sapiente sequi aut ducimus consequatur voluptatem aperiam! Rerum, amet. Aliquid esse odit id architecto sunt cumque, eius sit necessitatibus? Quis voluptatum harum maxime explicabo, illum voluptatem at!</p>
-            <p>Ducimus, amet tempore placeat debitis tempora iure earum, cumque officiis provident laudantium nemo sunt dolorum. Omnis harum rem, aliquam nam dignissimos quo autem repellendus iusto labore quam deserunt laudantium accusamus!</p>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Icons.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+function GithubIcon() {
+    return <FontAwesomeIcon className="center" size="lg" icon={faGithub}/> 
+}
+
+function LinkedInIcon() {
+    return <FontAwesomeIcon className="center" size="lg" icon={faLinkedin}/> 
+}
+
+function InstagramIcon() {
+    return <FontAwesomeIcon className="center" size="lg" icon={faInstagram}/> 
+}
+
+export { GithubIcon, LinkedInIcon, InstagramIcon };
